a3f90b0162a47775e8e320b5346795b62e68d726
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-26 13:20:30 +0530

    [Venkatesh] Add: Printing welcome message

---
+++ employee_wage.py
@@ -0,0 +1,19 @@
+'''
+
+@Author: Venkatesh
+@Date: 2024-08-26 18:00:30
+@Last Modified by: Author Name
+@Last Modified time: 2024-08-26 18:00:30
+@Title : Program Aim
+Python Program DocString Structure:
+
+''' 
+
+
+def main():
+    print("Welcome to EmployeeWage Computation Program on Master Branch")
+
+
+if __name__=="__main__":
+    main()
+
