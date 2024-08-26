41f0f2b06273435f70d329c09d5e7a9a529c95b0
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-26 15:36:47 +0530

    [Venkatesh] Add: Printing employee monthly wage

--- employee_wage.py
+++ employee_wage.py
@@ -15,6 +15,7 @@ import random
 WAGE_PER_HOUR = 20
 FULL_DAY_HOUR = 8
 PART_TIME_HOUR = 4
+WORK_DAYS=20
 
 def employee_attendance():
     '''
@@ -28,37 +29,40 @@ def employee_attendance():
     return random.randint(0,2)
     
 
-def daily_employee_wage(status,hours_per_day):
+def monthly_employee_wage(status,hours_per_day):
     '''
           Description: 
-                this function is calculating daily wage of employee
+                this function is calculating monthly wage for employee
           Parameters: 
+               status(int): 1:- employee is full time employee
+                            2:- employeee is part time employee
+                            0:- employee is absent
                hours_per_day : The number of hours the employee worked based on emp type i.e fulltime or parttime
           Return : 
                 Returns the wage for an employee
     '''
     match status:
         case 1:
-            return WAGE_PER_HOUR*hours_per_day
+            return WAGE_PER_HOUR*hours_per_day*WORK_DAYS
         case 2:
-            return WAGE_PER_HOUR* hours_per_day
+            return WAGE_PER_HOUR* hours_per_day*WORK_DAYS
         case 0:
             return 0
 
 
 
 def main():
-    print("Welcome to EmployeeWage Computation Program on Master Branch")
+    print("****Welcome to EmployeeWage Computation****")
     attendance = employee_attendance()
     if attendance == 1:
-        print(f'Employee is FULLTIME Employee')
-        print(f'Daily Employee Wage is : {daily_employee_wage(1,FULL_DAY_HOUR)}')
+        print(f'Employee is FULLTIME Employee wage for month')
+        print(f'Monthly Employee Wage is : {monthly_employee_wage(1,FULL_DAY_HOUR)}')
     elif attendance ==2:
         print(f'Employee is PARTTIME Employee')
-        print(f'Daily Employee Wage is : {daily_employee_wage(2,PART_TIME_HOUR)}')
+        print(f'Monthly Employee Wage is : {monthly_employee_wage(2,PART_TIME_HOUR)}')
     else:
         print(f'Employee is Absent')
-        print(f'Daily Employee Wage is : {0}')
+        print(f'monthly Employee Wage is : {0}')
 
 
 if __name__=="__main__":
