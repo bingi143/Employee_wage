c362610fd3f23448e2a7dc36b005a8426628693e
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-28 15:26:59 +0530

    [Venkatesh] Add: CRUD operations on company and employee

--- employee_wage.py
+++ employee_wage.py
@@ -92,6 +92,61 @@ class Company:
         employee_id = len(self.employees) + 1
         self.employees.append({'id': employee_id, 'name': employee_name})
 
+    def update_employee(self, employee_id, new_name):
+        '''
+        Description: 
+            Updates the name of an employee.
+        Parameters: 
+            employee_id: The ID of the employee to update.
+            new_name: The new name of the employee.
+        Return:
+            None
+        '''
+        for emp in self.employees:
+            if emp['id'] == employee_id:
+                emp['name'] = new_name
+                print(f"*** Employee ID {employee_id} name updated to {new_name} ***")
+                return
+        print(f"Employee with ID {employee_id} not found.")
+
+    def delete_employee(self, employee_id):
+        '''
+        Description: 
+            Deletes an employee from the company.
+        Parameters: 
+            employee_id: The ID of the employee to delete.
+        Return:
+            None
+        '''
+        for emp in self.employees:
+            if emp['id'] == employee_id:
+                self.employees.remove(emp)
+                print(f"*** Employee ID {employee_id} deleted ***")
+                return
+        print(f"Employee with ID {employee_id} not found.")
+
+    def update_company(self, new_name=None, new_wage_per_hour=None, new_total_work_days=None, new_total_work_hours=None):
+        '''
+        Description: 
+            Updates the company details.
+        Parameters: 
+            new_name: The new name of the company.
+            new_wage_per_hour: The new wage per hour.
+            new_total_work_days: The new total work days in a month.
+            new_total_work_hours: The new total work hours in a month.
+        Return:
+            None
+        '''
+        if new_name:
+            self.company_name = new_name
+        if new_wage_per_hour:
+            self.wage_per_hour = new_wage_per_hour
+        if new_total_work_days:
+            self.total_work_days_in_month = new_total_work_days
+        if new_total_work_hours:
+            self.total_work_hours_in_month = new_total_work_hours
+        print(f"*** Company details updated successfully ***")
+
 
 class CompanyEmpWage:
     def __init__(self, company, employee):
@@ -115,6 +170,20 @@ class EmpWageBuilder:
             company_emp_wage: The CompanyEmpWage object.
         '''
         self.company_emp_wages.append(company_emp_wage)
+
+    def delete_company(self, company_name):
+        '''
+        Description: 
+            Deletes a company and all associated employee wages.
+        Parameters: 
+            company_name: The name of the company to delete.
+        Return:
+            None
+        '''
+        for company_emp_wage in self.company_emp_wages:
+            if company_emp_wage.company.company_name == company_name:
+                self.company_emp_wages.remove(company_emp_wage)
+        print(f"*** Company {company_name} and all associated employee data deleted ***")
 
     def display_companies_and_employees(self):
         '''
@@ -160,9 +229,13 @@ def main():
             option = int(input('''Enter           
                 1: to add company 
                 2: to add employee to company
-                3: to display all companies and their employees
-                4: to display wages for all companies
-                5: to exit: '''))
+                3: to update employee in company
+                4: to delete employee from company
+                5: to update company details
+                6: to delete a company
+                7: to display all companies and their employees
+                8: to display wages for all companies
+                9: to exit: '''))
             
             if option == 1:
                 company_name = input("Enter the company name: ")
@@ -187,16 +260,61 @@ def main():
                     print(f"Company {company_name} not found.")
 
             elif option == 3:
-                emp_wage_builder.display_companies_and_employees()
+                company_name = input("Enter the company name: ")
+                employee_id = int(input("Enter the employee ID to update: "))
+                new_name = input("Enter the new employee name: ")
+                for company in companies:
+                    if company.company_name == company_name:
+                        company.update_employee(employee_id, new_name)
+                        break
+                else:
+                    print(f"Company {company_name} not found.")
 
             elif option == 4:
-                emp_wage_builder.display_wages()
+                company_name = input("Enter the company name: ")
+                employee_id = int(input("Enter the employee ID to delete: "))
+                for company in companies:
+                    if company.company_name == company_name:
+                        company.delete_employee(employee_id)
+                        break
+                else:
+                    print(f"Company {company_name} not found.")
 
             elif option == 5:
+                company_name = input("Enter the company name to update: ")
+                for company in companies:
+                    if company.company_name == company_name:
+                        new_name = input("Enter new company name (press Enter to skip): ")
+                        new_wage_per_hour = input("Enter new wage per hour (press Enter to skip): ")
+                        new_total_work_days = input("Enter new total work days per month (press Enter to skip): ")
+                        new_total_work_hours = input("Enter new total work hours per month (press Enter to skip): ")
+                        
+                        # Convert to appropriate types if provided
+                        new_wage_per_hour = int(new_wage_per_hour) if new_wage_per_hour else None
+                        new_total_work_days = int(new_total_work_days) if new_total_work_days else None
+                        new_total_work_hours = int(new_total_work_hours) if new_total_work_hours else None
+                        
+                        company.update_company(new_name, new_wage_per_hour, new_total_work_days, new_total_work_hours)
+                        break
+                else:
+                    print(f"Company {company_name} not found.")
+
+            elif option == 6:
+                company_name = input("Enter the company name to delete: ")
+                emp_wage_builder.delete_company(company_name)
+                companies = [company for company in companies if company.company_name != company_name]
+
+            elif option == 7:
+                emp_wage_builder.display_companies_and_employees()
+
+            elif option == 8:
+                emp_wage_builder.display_wages()
+
+            elif option == 9:
                 break
 
             else:
-                print("Invalid option. Please enter a number between 1 and 5.")
+                print("Invalid option. Please enter a number between 1 and 9.")
                
         except ValueError:
             print("Please enter a correct number")
